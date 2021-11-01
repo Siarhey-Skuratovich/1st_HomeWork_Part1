81ce36f42af84482ee9b6b546f62fc4e7165d57b
Сергей Скуратович <siarheyskuratovich@gmail.com>
2021-11-01 11:30:38 +0300

    added external lib lombok to the project

--- com/mainTask/Main.java
+++ com/mainTask/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Bicycle bicycle = new Bicycle();
+        bicycle.setBrand("Stels");
+        bicycle.setModel("Mountain bike");
+        bicycle.setUsed(true);
         bicycle.run();
     }
 }
